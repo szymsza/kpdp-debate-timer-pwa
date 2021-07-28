670d51edabe66cdbf0d4b2f28b6080ce4e69d145
Jakub Szymsza <jakub@szymsza.cz>
2021-07-28 18:21:00 +0200

    Radio component - events and props

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -1,18 +1,32 @@
 import { FunctionalComponent, h } from 'preact';
 
-const Radio: FunctionalComponent = () => (
+export interface RadioOption {
+  label: string
+  value: string
+  active: boolean
+}
+
+interface RadioProps {
+  label: string
+  options: RadioOption[]
+  onChange: (newValue: string) => void
+}
+
+const Radio: FunctionalComponent<RadioProps> = ({ label, options, onChange }) => (
   <section className="radio">
-    <p className="radio__label">Title:</p>
+    <p className="radio__label">{label}</p>
     <div className="radio__options">
-      <div className="radio__option radio__option--active">
-        Option 1
-      </div>
-      <div className="radio__option">
-        Option 2
-      </div>
-      <div className="radio__option">
-        Option 3
-      </div>
+      {
+        options.map((option: RadioOption) => (
+          <button
+            type="button"
+            className={`radio__option ${option.active ? 'radio__option--active' : ''}`}
+            onClick={() => onChange(option.value)}
+          >
+            {option.label}
+          </button>
+        ))
+      }
     </div>
   </section>
 );
--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,14 +1,44 @@
 import { FunctionalComponent, h } from 'preact';
+import { StateUpdater, useState } from 'preact/hooks';
 import Navbar from '../../components/Navbar';
-import Radio from '../../components/Radio';
+import Radio, { RadioOption } from '../../components/Radio';
 import About from '../../components/About';
 
-const Settings: FunctionalComponent = () => (
-  <main className="screen screen--settings">
-    <Navbar />
-    <Radio />
-    <About />
-  </main>
-);
+const setActiveColourMode = (newValue: string, setState: StateUpdater<RadioOption[]>): void => {
+  setState((old) => old.map((item) => ({
+    ...item,
+    active: item.value === newValue,
+  })));
+};
+
+const initialColourModes: RadioOption[] = [{
+  label: 'Auto',
+  value: 'auto',
+  active: true,
+}, {
+  label: 'Tmavý',
+  value: 'dark',
+  active: false,
+}, {
+  label: 'Světlý',
+  value: 'light',
+  active: false,
+}];
+
+const Settings: FunctionalComponent = () => {
+  const [colourModes, setColourModes] = useState<RadioOption[]>(initialColourModes);
+
+  return (
+    <main className="screen screen--settings">
+      <Navbar />
+      <Radio
+        label="Barevný režim:"
+        options={colourModes}
+        onChange={(newValue) => setActiveColourMode(newValue, setColourModes)}
+      />
+      <About />
+    </main>
+  );
+};
 
 export default Settings;
