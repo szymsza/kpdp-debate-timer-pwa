567555ee2da7be5f8809a2448cabf6d52363f1cd
Jakub Szymsza <jakub@szymsza.cz>
2021-07-29 21:20:49 +0200

    Global store TODOs

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -3,6 +3,24 @@ import { FunctionalComponent, h } from 'preact';
 import Timer from './Timer';
 import Settings from './Settings';
 
+/* TODO create global store
+    - screen
+        - timer/settings
+        - later connect to URL
+    - theme colour
+        - auto/light/dark
+        - add `data-theme` to #preact_root
+    - speakers
+        - map config:
+            - add `selected` property
+            - add `paused` property
+            - convert minutes to seconds
+    - prep times
+        - map config:
+            - add `active` property
+            - convert minutes to seconds
+*/
+
 const App: FunctionalComponent = () => (
   <div id="preact_root">
     <Timer />
