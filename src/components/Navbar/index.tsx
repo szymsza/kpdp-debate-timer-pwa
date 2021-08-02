dbac6e3428a3ed22f3522ba28a3ff145597a135a
Jakub Szymsza <jakub@szymsza.cz>
2021-08-02 12:34:33 +0200

    Add back icon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,11 @@
 import { FunctionalComponent, h } from 'preact';
+import Back from '../Icons/Back';
 
 const Navbar: FunctionalComponent = () => (
   <nav className="nav">
-    <button type="button" className="nav__back">&lt;</button>
+    <button type="button" className="nav__back">
+      <Back />
+    </button>
     <h1 className="nav__heading">
       Debatní stopky
     </h1>
