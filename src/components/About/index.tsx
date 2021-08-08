e15f5551b9ebad444959f7aed94aaacbf6ed9137
Jakub Szymsza <jakub@szymsza.cz>
2021-08-08 18:24:04 +0200

    Change logo colour with theme

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,17 +1,26 @@
 import { FunctionalComponent, h } from 'preact';
+import { useContext } from 'preact/hooks';
+import { Context } from '../../store';
 import AdkLogoLight from '../../assets/adk-logo.png';
 import AdkLogoDark from '../../assets/adk-logo-dark.png';
 
-const About: FunctionalComponent = () => (
-  <section className="about">
-    <p className="about__paragraph">
-      Jakékoliv připomínky či nápady neváhejte sdělit na:
-    </p>
-    <p className="about__paragraph about__paragraph--link">
-      <a href="mailto:info@debatovani.cz">info@debatovani.cz</a>
-    </p>
-    <img src={true ? AdkLogoLight : AdkLogoDark} alt="Logo" className="about__logo" />
-  </section>
-);
+const About: FunctionalComponent = () => {
+  const { state } = useContext(Context);
+
+  // TODO - deal with 'auto' value (+ use some global selector)
+  const isDarkTheme = state.themes.find((item) => item.value === 'dark')!.active;
+
+  return (
+    <section className="about">
+      <p className="about__paragraph">
+        Jakékoliv připomínky či nápady neváhejte sdělit na:
+      </p>
+      <p className="about__paragraph about__paragraph--link">
+        <a href="mailto:info@debatovani.cz">info@debatovani.cz</a>
+      </p>
+      <img src={isDarkTheme ? AdkLogoDark : AdkLogoLight} alt="Logo" className="about__logo" />
+    </section>
+  );
+};
 
 export default About;
