4b3d7c888b6de4df04b103805e0bbbd6204e29ec
Jakub Szymsza <jakub@szymsza.cz>
2021-08-08 18:46:42 +0200

    Render time component

--- src/components/Time/index.tsx
+++ src/components/Time/index.tsx
@@ -7,8 +7,24 @@ interface TimeProps {
   display: 'elapsed' | 'remaining'
 }
 
-const Time: FunctionalComponent<TimeProps> = ({ className }) => (
-  <span className={className ?? ''}>5:30</span>
+const getTimeString = (seconds: number): string => {
+  const secondsAbs = Math.abs(seconds);
+  const minutesPart = Math.floor(secondsAbs / 60);
+  const secondsPart = secondsAbs % 60;
+
+  return `${seconds < 0 ? '-' : ''}${minutesPart}:${secondsPart < 10 ? '0' : ''}${secondsPart}`;
+};
+
+const Time: FunctionalComponent<TimeProps> = ({
+  className, display, time,
+}) => (
+  <span className={className ?? ''}>
+    {getTimeString(
+      display === 'elapsed'
+        ? time.elapsed
+        : time.total - time.elapsed,
+    )}
+  </span>
 );
 
 export default Time;
