4cb9b475543ac5ef52742d7c473415260502e3dd
Jakub Szymsza <jakub@szymsza.cz>
2021-08-08 19:55:02 +0200

    Get active prep time function prepared

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,6 +1,10 @@
 import { StoreContent } from './types';
-import { SpeakerTimeSlot } from '../types';
+import { PrepTimeSlot, SpeakerTimeSlot } from '../types';
 
 export const getSelectedSpeaker = (state: StoreContent): SpeakerTimeSlot => (
   state.speakers.flat().find((speaker) => speaker.selected)!
+);
+
+export const getActivePrepTime = (state: StoreContent): PrepTimeSlot | undefined => (
+  state.prepTimes.find((time) => time.active)
 );
