6462f7d77268bc350b9edf338eada42715f5a1ab
Jakub Szymsza <jakub@szymsza.cz>
2021-08-08 18:36:07 +0200

    Render prep times and speakers from state

--- src/components/PrepTime/index.tsx
+++ src/components/PrepTime/index.tsx
@@ -1,30 +1,26 @@
 import { FunctionalComponent, h } from 'preact';
+import { useContext } from 'preact/hooks';
+import { Context } from '../../store';
 import Button from '../Button';
 
-const PrepTime: FunctionalComponent = () => (
-  <section className="preptime">
-    <h2 className="preptime__heading">Přípravný čas</h2>
-    <Button
-      title="Afirmace"
-      className="preptime__button"
-      inverse
-      time={{
-        label: 'Time',
-        total: 360,
-        elapsed: 12,
-      }}
-    />
-    <Button
-      title="Negace"
-      className="preptime__button"
-      inverse
-      time={{
-        label: 'Time',
-        total: 360,
-        elapsed: 12,
-      }}
-    />
-  </section>
-);
+const PrepTime: FunctionalComponent = () => {
+  const { state } = useContext(Context);
+
+  return (
+    <section className="preptime">
+      <h2 className="preptime__heading">Přípravný čas</h2>
+      {
+        state.prepTimes.map((time) => (
+          <Button
+            title={time.label}
+            className="preptime__button"
+            inverse
+            time={time}
+          />
+        ))
+      }
+    </section>
+  );
+};
 
 export default PrepTime;
--- src/components/Speakers/index.tsx
+++ src/components/Speakers/index.tsx
@@ -1,30 +1,31 @@
 import { FunctionalComponent, h } from 'preact';
+import { useContext } from 'preact/hooks';
+import { Context } from '../../store';
 import Button from '../Button';
 
-const Speakers: FunctionalComponent = () => (
-  <section className="speakers">
-    {
-      Array.from({ length: 2 }).map((_, index2) => (
-        <section className="speakers__column">
-          {
-            Array.from({ length: 5 }).map((_, index) => (
-              <Button
-                title={index > 2 ? 'N3 ➝ A1' : 'A1'}
-                className="speakers__button"
-                active={index === 2}
-                disabled={index === 3 || (index === 2 && index2 === 1)}
-                time={{
-                  label: 'Time',
-                  total: 360,
-                  elapsed: 12,
-                }}
-              />
-            ))
-          }
-        </section>
-      ))
-    }
-  </section>
-);
+const Speakers: FunctionalComponent = () => {
+  const { state } = useContext(Context);
+  return (
+    <section className="speakers">
+      {
+        state.speakers.map((party) => (
+          <section className="speakers__column">
+            {
+              party.map((speaker) => (
+                <Button
+                  title={speaker.label}
+                  className="speakers__button"
+                  active={speaker.selected}
+                  disabled={false}
+                  time={speaker}
+                />
+              ))
+            }
+          </section>
+        ))
+      }
+    </section>
+  );
+};
 
 export default Speakers;
