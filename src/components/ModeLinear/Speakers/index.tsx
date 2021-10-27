57d83cdb6639bb2316a05b09c805a044d9969288
Jakub Szymsza <jakub@szymsza.cz>
2021-10-28 00:03:24 +0200

    Linear mode top boxes

---
+++ src/components/ModeLinear/Speakers/index.tsx
@@ -0,0 +1,29 @@
+import { FunctionalComponent, h } from 'preact';
+import { useContext } from 'preact/hooks';
+import { Context } from '../../../store';
+import { getLinearSpeakersData } from '../../../store/getters';
+import { SpeakerTimeSlot } from '../../../types';
+import SpeakersBoxSlot from '../SpeakersBoxSlot';
+
+const Speakers: FunctionalComponent = () => {
+  const { store } = useContext(Context);
+  const speakersData: SpeakerTimeSlot[][][] = getLinearSpeakersData(store);
+
+  return (
+    <section className="speakers">
+      {
+        speakersData.map((party) => (
+          <section className={`speakers__column speakers__column--linear speakers__column--${party[0][0].party}`}>
+            {
+              party.map((timeSlots) => (
+                <SpeakersBoxSlot timeSlots={timeSlots} />
+              ))
+            }
+          </section>
+        ))
+      }
+    </section>
+  );
+};
+
+export default Speakers;
---
+++ src/components/ModeLinear/SpeakersBoxSlot/index.tsx
@@ -0,0 +1,25 @@
+import { FunctionalComponent, h } from 'preact';
+
+import { SpeakerTimeSlot } from '../../../types';
+import Time from '../../Time';
+
+interface SpeakerBoxSlotProps {
+  timeSlots: SpeakerTimeSlot[]
+}
+
+const SpeakersBoxSlot: FunctionalComponent<SpeakerBoxSlotProps> = ({
+  timeSlots,
+}) => (
+  <section className="speakers-box__slot">
+    {
+      timeSlots.map((slot, index) => (
+        <div className={`speakers-box__row speakers-box__row--${index === 0 ? 'speaker' : 'questioner'} ${slot.elapsed === 0 && slot.paused ? 'speakers-box__row--timeless' : '' }`}>
+          <span className="speakers-box__label">{slot.label}</span>
+          <Time className="speakers-box__time" time={slot} display="elapsed" />
+        </div>
+      ))
+    }
+  </section>
+);
+
+export default SpeakersBoxSlot;
