76ed4fce253bf7040f4ea72fe67e88b49601017e
Jakub Szymsza <jakub@szymsza.cz>
2023-04-23 22:20:35 +0200

    Make linear mode default

--- src/modes.ts
+++ src/modes.ts
@@ -2,7 +2,7 @@ import { getActiveOption } from './localStorage';
 
 export type Mode = 'linear' | 'classic';
 
-const defaultMode: Mode = 'classic';
+const defaultMode: Mode = 'linear';
 
 // Get active mode from local storage
 export const getActiveMode = (): Mode => <Mode>getActiveOption('mode', defaultMode);
